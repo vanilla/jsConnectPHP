3a7d2e12b654029bfd599a9b7ed85516ef5fb263
Todd Burry <todd@vanillaforums.com>
2012-02-12 08:31:28 -0500

    Removed !defined('APPLICATION') line.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vanilla jsConnect Client Library for PHP #
+
+This repository contains the files you need to use Vanilla's jsConnect with a PHP project.
+
+The following files are included in this repo.
+
+* [`functions.jsconnect.php`](https://github.com/vanillaforums/jsConnectPHP/blob/master/functions.jsconnect.php)
+  This is the main file you need. You don't need any other file in your project. You can just drop this file anywhere that you can access it on your site.
+* [`index.php`](https://github.com/vanillaforums/jsConnectPHP/blob/master/index.php)
+  This file offers an example usage. You can customize this page or start from scratch.
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require_once dirname(__FILE__).'/functions.jsconnect.php';;
+
+// 1. Get your client ID and secret here. These must match those in your jsConnect settings.
+$clientID = "1234";
+$secret = "1234";
+
+// 2. Grab the current user from your session management system or database here.
+$signedIn = true; // this is just a placeholder
+
+// YOUR CODE HERE.
+
+// 3. Fill in the user information in a way that Vanilla can understand.
+$user = array();
+
+if ($signedIn) {
+   // CHANGE THESE FOUR LINES.
+   $user['uniqueid'] = '123';
+   $user['name'] = 'John PHP';
+   $user['email'] = 'john.php@anonymous.com';
+   $user['photourl'] = '';
+}
+
+// 4. Generate the jsConnect string.
+
+// This should be true unless you are testing. 
+// You can also use a hash name like md5, sha1 etc which must be the name as the connection settings in Vanilla.
+$secure = true; 
+WriteJsConnect($user, $_GET, $clientID, $secret, $secure);
